fcb64035eb3a44b6103ccdb959f5ef35e8d92266
Ariel Isaacs <ariel.isaacs@gmail.com>
2015-01-22 22:28:11 -0800

    initalizing twitter works

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,17 +1,17 @@
 class Follower < ActiveRecord::Base
 
-  def get_connection
+
+  def initialize_twitter
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = "8uNN8y5huLbCmhrPgFYAtyQU1"
       config.consumer_secret     = "GkUWjxPN4o2p27i45rZ86uCEFUtGCmGvcSyVK79GihMK8BzYnY"
       config.access_token        = "58889022-AJxRxt0AttYy6Vr8V9bquyWUXnWzVmGOpEU2YZVwq"
-      config.access_token_secret = "zQhLxD4wXQxVbWL9aVoS1XQ1Sft1PXuITgZ7tUEuesCVi"    
-    end
+      config.access_token_secret = "zQhLxD4wXQxVbWL9aVoS1XQ1Sft1PXuITgZ7tUEuesCVi"   
+    end 
   end
 
+
   def get_friends
-    connection = InitializeTwitter.new
-    @client = connection.new_client()
     following_array = [];
     cursor = -1
     while cursor != 0
@@ -20,5 +20,11 @@ class Follower < ActiveRecord::Base
       following_array.concat(response.to_a)
     end
   end
+
+
 end
+
+
+
+
 
